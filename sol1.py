2c836395070224044ef9eb0770e2051e3d7cbe49
herry <guoqh670@gmail.com>
2024-05-12 18:52:27 +0800

    Algorithm 1&2

---
+++ sol1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findMinSteps(self, ring: str, key: str) -> int:
+
+        hs = dict()
+        for i in range(len(ring)):
+            if ring[i] not in hs:
+                hs[ring[i]] = [i]
+            else:
+                hs[ring[i]].append(i)
+
+        m, n = len(ring), len(key)
+        dp = [float('inf')] * m
+        for q in hs[key[0]]:
+            dp[q] = min(q, m - q) + 1
+
+        for i in range(1, n):
+            if key[i] != key[i - 1]:
+                for p in hs[key[i - 1]]:
+                    for j in hs[key[i]]:
+                        dp[j] = min(dp[j], min(abs(j - p), m - abs(j - p)) + dp[p] + 1)
+                    dp[p] = float('inf')
+            else:
+                for j in hs[key[i]]:
+                    dp[j] += 1
+        return min(dp)
---
+++ sol2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findRotateSteps(self, s: str, t: str) -> int:
+        n, m = len(s), len(t)
+        vis = [[False] * (m + 1) for _ in range(n)]
+        vis[0][0] = True
+        q = [(0, 0)]
+        step = 0
+        while True:
+            tmp = q
+            q = []
+            for i, j in tmp:
+                if j == m:
+                    return step
+                if s[i] == t[j]:
+                    if not vis[i][j + 1]:
+                        vis[i][j + 1] = True
+                        q.append((i, j + 1))
+                    continue
+                for i2 in (i - 1) % n, (i + 1) % n:
+                    if not vis[i2][j]:
+                        vis[i2][j] = True
+                        q.append((i2, j))
+            step += 1
